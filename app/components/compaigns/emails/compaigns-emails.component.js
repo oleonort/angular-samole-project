e6cfd98db66b3218ca614a41d26d2a532409cb7e
oleonort <oleonortt@gmail.com>
2016-09-12 17:45:57 +0200

    compaigns-email (part)

--- app/components/compaigns/emails/compaigns-emails.component.js
+++ app/components/compaigns/emails/compaigns-emails.component.js
@@ -3,5 +3,12 @@
 // Define the `compaignsEmailsComponent` module
 angular.module('compaignsEmails')
     .component('compaignsEmails', {
-        templateUrl: 'components/compaigns/emails/compaigns-emails.html'
+        templateUrl: 'components/compaigns/emails/compaigns-emails.html',
+        controller: ['$http',
+            function CompaignsEmailsController($http) {
+                $http.get('data/compaigns-email.json').then(function(response) {
+                    this.overalls = response.data.overalls;
+                }.bind(this));
+            }
+        ]
     });
