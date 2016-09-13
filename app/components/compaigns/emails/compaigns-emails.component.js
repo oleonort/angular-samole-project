559e5bbaba627473f9f22b6df360a8d1ff2f0281
Artur <oleonortt@gmail.com>
2016-09-13 18:13:54 +0200

    compaigns-email

--- app/components/compaigns/emails/compaigns-emails.component.js
+++ app/components/compaigns/emails/compaigns-emails.component.js
@@ -4,10 +4,12 @@
 angular.module('compaignsEmails')
     .component('compaignsEmails', {
         templateUrl: 'components/compaigns/emails/compaigns-emails.html',
-        controller: ['$http',
-            function CompaignsEmailsController($http) {
+        controller: ['$http', '$timeout',
+            function CompaignsEmailsController($http, $timeout) {
                 $http.get('data/compaigns-email.json').then(function(response) {
                     this.overalls = response.data.overalls;
+                    this.emailItems = response.data.emailItems;
+                    this.emailItemsDropdown = response.data.emailItemsDropdown;
                 }.bind(this));
 
                 $http.get('data/dropdown-groups.json').then(function(response) {
@@ -17,6 +19,10 @@ angular.module('compaignsEmails')
                 $http.get('data/dropdown-statuses.json').then(function(response) {
                     this.statusesDropdownData = response.data;
                 }.bind(this));
+
+                this.$doCheck = function() {
+                    $('[data-toggle="tooltip"]').tooltip();
+                }
             }
         ]
     });
