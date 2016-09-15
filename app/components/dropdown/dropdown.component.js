5470550af0dfabc1a5d133af7a6e39d0415d3f69
Artur <oleonortt@gmail.com>
2016-09-15 23:48:56 +0200

    css

--- app/components/dropdown/dropdown.component.js
+++ app/components/dropdown/dropdown.component.js
@@ -8,9 +8,18 @@ angular.module('customDropdown')
             dropdownData: '<'
         },
         templateUrl: 'components/dropdown/dropdown.html',
-        controller: function customDropdownController () {
+        controller: function customDropdownController() {
             this.$onChanges = function (changesObj) {
                 this.dropdownData = changesObj.dropdownData.currentValue ? changesObj.dropdownData.currentValue : [];
+            };
+
+            this.$doCheck = function () {
+                $('.special-dropdown .secondary-button').hover(
+                    function () {
+                        $(this).siblings(".main-button").css({'border-color': '#2A73D5'});
+                    }, function () {
+                        $(this).siblings(".main-button").css({'border-color': '#DBDFE4'});
+                    });
             };
         }
     });
